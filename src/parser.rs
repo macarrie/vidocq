aa30d1815ede1d29d1f84872f9e5155f73b91c84
macarrie <mathieu0carrie@gmail.com>
2018-06-07 18:21:06 +0200

    Refactored regex matching using rust option type methods

--- src/parser.rs
+++ src/parser.rs
@@ -9,35 +9,16 @@ pub struct MediaInfo {
 }
 
 fn parse_episode(name :&str) -> (i32, i32) {
-    let season :i32;
-    let episode :i32;
-
     lazy_static! {
         static ref RE_SEASON :Regex = Regex::new(r"(?i)(s)?(?P<season>\d{1,2})[ex]").unwrap();
         static ref RE_EPISODE :Regex = Regex::new(r"(?i)[ex]p?(?P<episode>\d{1,2})").unwrap();
     }
 
-    let season_search = RE_SEASON.captures(name);
-    match season_search {
-        Some(x) => {
-            let season_nb :i32 = x["season"].to_string().parse().unwrap();
-            season = season_nb;
-        },
-        None => {
-            season = 0;
-        }
-    }
+    let season :i32 = RE_SEASON.captures(name)
+        .map_or(0, |x| x["season"].to_string().parse::<i32>().unwrap_or(0));
 
-    let episode_search = RE_EPISODE.captures(name);
-    match episode_search {
-        Some(x) => {
-            let episode_nb :i32 = x["episode"].to_string().parse().unwrap();
-            episode = episode_nb;
-        },
-        None => {
-            episode = 0;
-        }
-    }
+    let episode :i32 = RE_EPISODE.captures(name)
+        .map_or(0, |x| x["episode"].to_string().parse::<i32>().unwrap_or(0));
 
     (season, episode)
 }
@@ -47,16 +28,8 @@ fn parse_quality(name :&str) -> i32 {
         static ref RE_QUALITY :Regex = Regex::new(r"(?i)(?P<quality>\d{3,4})[pi]").unwrap();
     }
 
-    let quality_search = RE_QUALITY.captures(name);
-    match quality_search {
-        Some(x) => {
-            let quality :i32 = x["quality"].to_string().parse().unwrap();
-            quality
-        },
-        None => {
-            0
-        }
-    }
+    RE_QUALITY.captures(name)
+        .map_or(0, |x| x["quality"].to_string().parse::<i32>().unwrap_or(0))
 }
 
 fn parse_year(name :&str) -> i32 {
@@ -64,16 +37,8 @@ fn parse_year(name :&str) -> i32 {
         static ref RE_YEAR :Regex = Regex::new(r"(?P<year>19\d{2}|20\d{2})").unwrap();
     }
 
-    let year_search = RE_YEAR.captures(name);
-    match year_search {
-        Some(x) => {
-            let year :i32 = x["year"].to_string().parse().unwrap();
-            year
-        },
-        None => {
-            0
-        }
-    }
+    RE_YEAR.captures(name)
+        .map_or(0, |x| x["year"].to_string().parse::<i32>().unwrap_or(0))
 }
 
 pub fn parse(name :&str) -> MediaInfo {
@@ -98,18 +63,18 @@ mod tests {
     #[test]
     fn test_parse_episode() {
         let testlist = [ "+2x5",
-            "+2X5",
-            "+02x05",
-            "+2X05",
-            "+02x5",
-            "S02E05",
-            "s02e05",
-            "s02e5",
-            "s2e05",
-            "s02ep05",
-            "s2EP5",
-            "-s02e05",
-            "-2x05"];
+        "+2X5",
+        "+02x05",
+        "+2X05",
+        "+02x5",
+        "S02E05",
+        "s02e05",
+        "s02e5",
+        "s2e05",
+        "s02ep05",
+        "s2EP5",
+        "-s02e05",
+        "-2x05"];
 
         for s in testlist.iter() {
             println!("Test item: {}", s);
