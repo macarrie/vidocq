ed4b2c4701ab0cb8a963014f9c4dca1834c6621b
macarrie <mathieu0carrie@gmail.com>
2018-08-08 17:04:33 +0200

    WIP: Add release_group detection

---
+++ src/release_group.rs
@@ -0,0 +1,35 @@
+use regex::Regex;
+
+pub fn parse(name :&str) -> String {
+    lazy_static! {
+        static ref RE_RELEASE_GROUP :Regex = Regex::new(r".*- ?(?P<group>.*)").unwrap();
+    }
+
+    let mut group :String = String::from("");
+    for capture in RE_RELEASE_GROUP.captures_iter(name) {
+        group = capture["group"].to_string();
+        println!("GROUP: {}", group)
+    }
+
+    return group.trim().to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use super::*;
+
+    #[test]
+    fn test_parse_year() {
+        let mut test_grid :HashMap<&str, &str> = HashMap::new();
+        test_grid.insert("Movie.Name.2018 - ReleaseGroup", "ReleaseGroup");
+
+        for (key, val) in test_grid {
+            println!("Test item: {}", key);
+            let group = parse(key);
+            println!("Expected value: {}, result: {}", val, group);
+
+            assert!(group == val);
+        }
+    }
+}
